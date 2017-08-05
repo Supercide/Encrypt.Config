84bd5659afe55fb42b27e57b187af1db5ffa3cd5
Jordan anderson <jordan_anders@live.co.uk>
2017-08-05 20:41:11 +0100

    Moves json operations to own project

---
+++ src/Encrypt.Config.Json/JsonConfigurationFileEncrypter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Encrypt.Config.RSA;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Encrypt.Config.Json
+{
+    public class JsonConfigurationFileEncrypter : IDisposable
+    {
+        private readonly RSAWrapper _rsaWrapper;
+        private readonly RandomNumberGenerator _rng;
+
+        public JsonConfigurationFileEncrypter(RSAWrapper RSAWrapper)
+        {
+            _rsaWrapper = RSAWrapper;
+            _rng = new RNGCryptoServiceProvider();
+        }
+
+        public string Encrypt(string jsonConfigPath)
+        {
+            JsonConfigurationFileParser parser = new JsonConfigurationFileParser();
+            JsonReader reader = new JsonReader();
+
+            var obj = reader.Read(File.Open(jsonConfigPath, FileMode.Open));
+
+            var parsedJson = parser.Parse(obj);
+
+            foreach (var kvp in parsedJson)
+            {
+                var bytes = Encoding.UTF8.GetBytes(kvp.Value);
+                byte[] salt = new byte[24];
+                _rng.GetBytes(salt);
+                var encryptedBytes = _rsaWrapper.Encrypt(bytes, salt);
+
+                string base64 = Convert.ToBase64String(encryptedBytes);
+                string saltBase64 = Convert.ToBase64String(salt);
+
+                string[] properties = kvp.Key.Split('.');
+
+                JToken current = obj;//[properties[0]];
+
+                for (var i = 0; i < properties.Length; i++)
+                {
+                    current = current[properties[i]];
+                    if (i == properties.Length - 2)
+                    {
+                        current[properties[i + 1]] = $"{base64}.{saltBase64}";
+                    }
+                }
+            }
+
+           return JsonConvert.SerializeObject(obj);
+        }
+
+        public void Dispose()
+        {
+            _rsaWrapper?.Dispose();
+            _rng?.Dispose();
+        }
+    }
+}
---
+++ src/Encrypt.Config.Json/JsonConfigurationFileParser.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Encrypt.Config.Json
+{
+    internal class JsonConfigurationFileParser
+    {
+        private readonly IDictionary<string, string> _data = new SortedDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        private readonly Stack<string> _context = new Stack<string>();
+        private string _currentPath;
+
+        public IDictionary<string, string> Parse(JObject jsonConfig)
+        {
+            _data.Clear();
+
+            VisitJObject(jsonConfig);
+
+            return _data;
+        }
+
+        private void VisitJObject(JObject jObject)
+        {
+            foreach (var property in jObject.Properties())
+            {
+                EnterContext(property.Name);
+                VisitProperty(property);
+                ExitContext();
+            }
+        }
+
+        private void VisitProperty(JProperty property)
+        {
+            VisitToken(property.Value);
+        }
+
+        private void VisitToken(JToken token)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    VisitJObject(token.Value<JObject>());
+                    break;
+
+                case JTokenType.Array:
+                    VisitArray(token.Value<JArray>());
+                    break;
+
+                case JTokenType.Integer:
+                case JTokenType.Float:
+                case JTokenType.String:
+                case JTokenType.Boolean:
+                case JTokenType.Bytes:
+                case JTokenType.Raw:
+                case JTokenType.Null:
+                    VisitPrimitive(token.Value<JValue>());
+                    break;
+
+                default:
+                    throw new FormatException("FormatError Unsupported JSONToken");
+            }
+        }
+
+        private void VisitArray(JArray array)
+        {
+            for (int index = 0; index < array.Count; index++)
+            {
+                EnterContext(index.ToString());
+                VisitToken(array[index]);
+                ExitContext();
+            }
+        }
+
+        private void VisitPrimitive(JValue data)
+        {
+            var key = _currentPath;
+
+            if (_data.ContainsKey(key))
+            {
+                throw new FormatException($"Key Is Duplicated key: {key}");
+            }
+            _data[key] = data.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void EnterContext(string context)
+        {
+            _context.Push(context);
+            _currentPath = string.Join(".", _context.Reverse());
+        }
+
+        private void ExitContext()
+        {
+            _context.Pop();
+            _currentPath = string.Join(".", _context.Reverse());
+        }
+    }
+}
