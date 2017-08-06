41206b87bc3f2642c9e5a6b4a6d313f2f81ffad0
Jordan anderson <jordan_anders@live.co.uk>
2017-08-06 03:17:12 +0100

    Refactors Program main to be more structured

--- src/Encrypt.Config.Console/Program.cs
+++ src/Encrypt.Config.Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Encrypt.Config.Json;
 using Encrypt.Config.RSA;
 using Newtonsoft.Json;
@@ -11,60 +12,75 @@ namespace Encrypt.Config.Console
 {
     public class Program
     {
+        public const string COMMAND_DELIMINATOR = "-";
+
         public static void Main(string[] args)
         {
-            if(args.Contains("-jc") && args.Contains("-pbo"))
+            Dictionary<string, string> context = ToCommands(args);
+
+            if (context.ContainsKey(WellKnownCommands.JSON_CONFIGURATION_PATH) && 
+                context.ContainsKey(WellKnownCommands.EXPORT_PUBLIC_KEY))
             {
                 throw new InvalidOperationException();
             }
 
-            Dictionary<string, string> commands = new Dictionary<string, string>();
-
-            for (var index = 0; index < args.Length; index+=2)
+            if(context.ContainsKey(WellKnownCommands.PUBLIC_KEY_PATH))
             {
-                commands.Add(args[index], args[index+1]);
-            }
-
-            if(commands.ContainsKey("-pbi"))
-            {
-                var publicKey = File.ReadAllText(commands["-pbi"]);
-
-                using (var wrapper = RSAContainerFactory.CreateFromPublicKey(commands["-n"], publicKey, commands["-u"]))
-                using (var fileEncrypter = new JsonConfigurationFileEncrypter(wrapper))
-                {
-                    if(commands.ContainsKey("-jc"))
-                    {
-                        using (var reader = new JsonTextReader(new StreamReader(commands["-jc"])))
-                        {
-                            JObject jsonEncrypted = fileEncrypter.Encrypt(JObject.Load(reader));
-
-                            File.WriteAllText("appsettings.encrypted.json", jsonEncrypted.ToString(Formatting.None));
-                        }
-                    }
-                }
+                EncryptConfigurationFromPublicKey(context);
             } else
             {
-                using (var wrapper = RSAContainerFactory.Create(commands["-n"], commands["-u"]))
-                    using (var fileEncrypter = new JsonConfigurationFileEncrypter(wrapper))
-                    {
-                        var export = wrapper.Export(false);
-
-                        if (commands.ContainsKey("-jc"))
-                        {
-                            using (var reader = new JsonTextReader(new StreamReader(commands["-jc"])))
-                            {
-                                JObject jsonEncrypted = fileEncrypter.Encrypt(JObject.Load(reader));
-
-                                File.WriteAllText("appsettings.encrypted.json", jsonEncrypted.ToString(Formatting.None));
-                            }
-                        }
-
-                        if (commands.ContainsKey("-pbo"))
-                        {
-                            File.WriteAllText(commands["-pbo"], export);
-                        }
-                    }
+                ExportPublicKey(context);
             }
+        }
+
+        private static void ExportPublicKey(Dictionary<string, string> context)
+        {
+            using (var wrapper = RSAContainerFactory.Create(context[WellKnownCommands.SET_CONTAINER_NAME], context[WellKnownCommands.SET_USERNAME]))
+            {
+                var export = wrapper.Export(false);
+
+                if(context.ContainsKey(WellKnownCommands.EXPORT_PUBLIC_KEY))
+                {
+                    File.WriteAllText(context[WellKnownCommands.EXPORT_PUBLIC_KEY], export);
+                }
+            }
+        }
+
+        private static void EncryptConfigurationFromPublicKey(Dictionary<string, string> commands)
+        {
+            var publicKey = File.ReadAllText(commands[WellKnownCommands.PUBLIC_KEY_PATH]);
+
+            using (var wrapper = RSAContainerFactory.CreateFromPublicKey(commands[WellKnownCommands.SET_CONTAINER_NAME], publicKey, commands[WellKnownCommands.SET_USERNAME]))
+            using (var fileEncrypter = new JsonConfigurationFileEncrypter(wrapper))
+            {
+                if(commands.ContainsKey(WellKnownCommands.JSON_CONFIGURATION_PATH))
+                {
+                    using (var reader = new JsonTextReader(new StreamReader(commands[WellKnownCommands.JSON_CONFIGURATION_PATH])))
+                    {
+                        JObject jsonEncrypted = fileEncrypter.Encrypt(JObject.Load(reader));
+
+                        File.WriteAllText("appsettings.encrypted.json", jsonEncrypted.ToString(Formatting.None));
+                    }
+                }
+            }
+        }
+
+        private static Dictionary<string, string> ToCommands(string[] args)
+        {
+            string[] commandsWithArguments = Regex.Split(string.Join(" ", args), "(^-|\\s-)")
+                                     .Where(x => x.Trim() != "-" && !string.IsNullOrWhiteSpace(x))
+                                     .ToArray();
+
+            Dictionary<string, string> commandDictionary = new Dictionary<string, string>();
+
+            foreach (var commandLine in commandsWithArguments)
+            {
+                var sections = commandLine.Split(' ');
+
+                commandDictionary.Add(sections[0], string.Join(" ", sections.Skip(1)));
+            }
+
+            return commandDictionary;
         }
     }
 }
---
+++ src/Encrypt.Config.Console/WellKnownCommands.cs
@@ -0,0 +1,10 @@
+namespace Encrypt.Config.Console {
+    public class WellKnownCommands
+    {
+        public const string JSON_CONFIGURATION_PATH = "jc";
+        public const string EXPORT_PUBLIC_KEY = "po";
+        public const string PUBLIC_KEY_PATH = "pi";
+        public const string SET_CONTAINER_NAME = "n";
+        public const string SET_USERNAME = "u";
+    }
+}
