53ee8954733e218d21115a189d03d3967929492b
Jordan anderson <jordan_anders@live.co.uk>
2017-08-06 01:39:38 +0100

    Don't allow exporting and encryption in the same command

--- src/Encrypt.Config.Console/Program.cs
+++ src/Encrypt.Config.Console/Program.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Encrypt.Config.Json;
 using Encrypt.Config.RSA;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Encrypt.Config.Console
 {
@@ -9,6 +13,11 @@ namespace Encrypt.Config.Console
     {
         public static void Main(string[] args)
         {
+            if(args.Contains("-jc") && args.Contains("-pbo"))
+            {
+                throw new InvalidOperationException();
+            }
+
             Dictionary<string, string> commands = new Dictionary<string, string>();
 
             for (var index = 0; index < args.Length; index+=2)
@@ -16,22 +25,45 @@ namespace Encrypt.Config.Console
                 commands.Add(args[index], args[index+1]);
             }
 
-            using (var wrapper = RSAContainerFactory.Create(commands["-n"], commands["-u"]))
-            using (var fileEncrypter = new JsonConfigurationFileEncrypter(wrapper))
+            if(commands.ContainsKey("-pbi"))
             {
-                var export = wrapper.Export(false);
+                var publicKey = File.ReadAllText(commands["-pbi"]);
 
-                if(commands.ContainsKey("-jc"))
+                using (var wrapper = RSAContainerFactory.CreateFromPublicKey(commands["-n"], publicKey, commands["-u"]))
+                using (var fileEncrypter = new JsonConfigurationFileEncrypter(wrapper))
                 {
-                    var jsonEncrypted = fileEncrypter.Encrypt(commands["-jc"]);
+                    if(commands.ContainsKey("-jc"))
+                    {
+                        using (var reader = new JsonTextReader(new StreamReader(commands["-jc"])))
+                        {
+                            JObject jsonEncrypted = fileEncrypter.Encrypt(JObject.Load(reader));
 
-                    File.WriteAllText("appsettings.encrypted.json", jsonEncrypted);
+                            File.WriteAllText("appsettings.encrypted.json", jsonEncrypted.ToString(Formatting.None));
+                        }
+                    }
                 }
+            } else
+            {
+                using (var wrapper = RSAContainerFactory.Create(commands["-n"], commands["-u"]))
+                    using (var fileEncrypter = new JsonConfigurationFileEncrypter(wrapper))
+                    {
+                        var export = wrapper.Export(false);
 
-                if (commands.ContainsKey("-pbo"))
-                {
-                    File.WriteAllText(commands["-pbo"], export.Key);
-                }
+                        if (commands.ContainsKey("-jc"))
+                        {
+                            using (var reader = new JsonTextReader(new StreamReader(commands["-jc"])))
+                            {
+                                JObject jsonEncrypted = fileEncrypter.Encrypt(JObject.Load(reader));
+
+                                File.WriteAllText("appsettings.encrypted.json", jsonEncrypted.ToString(Formatting.None));
+                            }
+                        }
+
+                        if (commands.ContainsKey("-pbo"))
+                        {
+                            File.WriteAllText(commands["-pbo"], export);
+                        }
+                    }
             }
         }
     }
